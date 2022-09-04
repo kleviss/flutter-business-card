56a7a1eb144d9725ce272df21dc8c14cb801700a
Klevis macXhyra <klevis.xhyra@mova.ie>
2022-09-05 01:25:45 +0200

    appbar with Spitch color

--- lib/main.dart
+++ lib/main.dart
@@ -13,130 +13,17 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        backgroundColor: Colors.black12,
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/klevis.jpeg'),
-                ),
-                Text(
-                  'Klevis X.',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.0,
-                    fontFamily: 'Pacifico',
-                  ),
-                ),
-                Text(
-                  'Flutter Developer',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.0,
-                    fontFamily: 'IndieFlower',
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                  width: 150.0,
-                  child: Divider(
-                    color: Colors.white,
-                  ),
-                ),
-                Card(
-                  color: Colors.white,
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.black,
-                    ),
-                    title: Text(
-                      '+49 160 326 5882',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                        fontFamily: 'IndieFlower',
-                      ),
-                    ),
-                  ),
-                ),
-                Card(
-                  color: Colors.white,
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.black,
-                    ),
-                    title: Text(
-                      'klevis@spitch.live',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                        fontFamily: 'IndieFlower',
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+        appBar: AppBar(
+          title: const Text('Player Detail Page',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Lato',
+                fontWeight: FontWeight.bold,
+              )),
+          backgroundColor: const Color(0xFF1DB954),
         ),
+        backgroundColor: Colors.black12,
       ),
     );
   }
 }
-
-// FIRST HELLO WORLD APP
-// return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: Scaffold(
-//         backgroundColor: Colors.black12,
-//         appBar: AppBar(
-//           title: const Text('Flutter'),
-//           backgroundColor: Colors.blueGrey[900],
-//         ),
-//         body: Center(
-//           child: Column(
-//             children: const <Widget>[
-//               Text(
-//                 'Hello Word!!',
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 30.0,
-//                   fontWeight: FontWeight.bold,
-//                   letterSpacing: 2.0,
-//                   fontFamily: 'IndieFlower',
-//                 ),
-//               ),
-//               Text(
-//                 'I am a Flutter Developer',
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 20.0,
-//                   fontWeight: FontWeight.bold,
-//                   letterSpacing: 2.0,
-//                   fontFamily: 'IndieFlower',
-//                 ),
-//               ),
-//               Image(image: NetworkImage('https://picsum.photos/250?image=1')),
-//               Image(image: AssetImage('images/devlife.jpg'))
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
