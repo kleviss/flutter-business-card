20c580b4ccc4db7cd7f196e44b6f805df189fb42
Klevis macXhyra <klevis.xhyra@mova.ie>
2022-09-05 01:27:16 +0200

    created Page header

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,69 @@ class MyApp extends StatelessWidget {
           backgroundColor: const Color(0xFF1DB954),
         ),
         backgroundColor: Colors.black12,
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: const <Widget>[
+                      CircleAvatar(
+                        radius: 50.0,
+                        backgroundImage: AssetImage('images/klevis.jpeg'),
+                      ),
+                      Text(
+                        'Klevis X.',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.0,
+                          fontFamily: 'Lato',
+                        ),
+                      ),
+                      Chip(
+                        label: Text('Goalkeeper'),
+                        padding: EdgeInsets.all(8.0),
+                        visualDensity: VisualDensity.compact,
+                        backgroundColor: Colors.white,
+                        shape: StadiumBorder(
+                          side: BorderSide(
+                            color: Color(0xFF1DB954),
+                            width: 1.0,
+                          ),
+                        ),
+                        avatar: CircleAvatar(
+                          backgroundColor: Colors.black,
+                          child: Icon(
+                            Icons.sports_soccer,
+                            color: Colors.white,
+                          ),
+                        ),
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 10.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.0,
+                          fontFamily: 'Lato',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 0.0,
+                        width: 50.0,
+                        // child: Divider(
+                        //   color: Colors.white,
+                        // ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
