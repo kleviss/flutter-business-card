f43b54c08ca2bf1be6423ca7b896c35e092ee225
Klevis macXhyra <klevis.xhyra@mova.ie>
2022-09-08 20:18:05 +0200

    widet to render list of widgets

--- lib/scoreRow.dart
+++ lib/scoreRow.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'widgets/neo_text.dart';
 
 class ScoreRow extends StatelessWidget {
-  final Widget? child;
+  final List<Widget> children;
 
-  const ScoreRow({super.key, required this.child});
+  const ScoreRow({super.key, required this.children});
 
   @override
   Widget build(BuildContext context) {
-    // Material is a conceptual piece
-    // of paper on which the UI appears.
-    return (child!);
+    return (Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: children,
+      // add the children here
+    ));
   }
 }
